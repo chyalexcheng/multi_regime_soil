b192d554f5d3a9a6ea3341648572497a5f4a6584
chyalexcheng <chyalexcheng@gmail.com>
2024-07-24 03:33:58 +0200

    a first working version of MCC for fluid-solid transition

--- MCC.py
+++ MCC.py
@@ -13,8 +13,8 @@ nu = 0.15  # 'Poisson ratio'
 # Initial conditions
 p0 = 150.0  # 'Initial confining pressure [kPa]'
 V = N - (lambda_val * np.log(pc_0)) + (kappa * np.log(pc_0 / p0))  # Specific Volume
-e0 = V - 1  # Initial void ratio
-Phi_0 = 1.0 / (1.0 + e0)  # Initial solid volume fraction
+void_ratio_0 = V - 1  # Initial void ratio
+Phi_0 = 1.0 / (1.0 + void_ratio_0)  # Initial solid volume fraction
 deformation_mode = 'drained'
 
 # Maximum shear strain and number of load steps for the quasi-static stage
@@ -24,17 +24,17 @@ load_length = int(1e5)  # [-] 'loadsteps'
 dt = time / load_length
 
 # Collisional contribution parameters
-Delta_Phi = 1.0
+Delta_Phi = 4e-4
 M_c = 1.8
 
 # Define a loading history
-eqp_inc = eqp_tot / (1e2 * load_length)  # [-] 'increment applied plastic Plastic shear strain'
+eqp_inc = eqp_tot / (1e2 * load_length)  # [-] 'incrementl applied plastic shear strain'
 eqp_inc_history = np.concatenate([
     np.ones(round(1.0 / eqp_inc)) * eqp_inc,
-    # np.linspace(eqp_inc, 100 * eqp_inc, round(1.0 / eqp_inc)),
-    # np.ones(round(1.0 / eqp_inc)) * 100 * eqp_inc,
-    # np.linspace(100 * eqp_inc, eqp_inc, round(0.5 / eqp_inc)),
-    # np.ones(round(0.5 / eqp_inc)) * eqp_inc,
+    np.linspace(eqp_inc, 100 * eqp_inc, round(0.5 / eqp_inc)),
+    np.ones(round(1.0 / eqp_inc)) * 100 * eqp_inc,
+    np.linspace(100 * eqp_inc, eqp_inc, round(0.5 / eqp_inc)),
+    np.ones(round(0.5 / eqp_inc)) * eqp_inc,
 ])
 load_length = eqp_inc_history.shape[0]
 
@@ -44,49 +44,65 @@ plt.xlabel('Time [s]')
 plt.ylabel('Shear strain rate [1/s]')
 
 # Declarations
+void_ratio_total = np.zeros(load_length)
+void_ratio_q = np.zeros(load_length)
 p = np.zeros(load_length)
 q = np.zeros(load_length)
 u = np.zeros(load_length)
 ev = np.zeros(load_length)
 eq = np.zeros(load_length)
-e = np.zeros(load_length)
-psi = np.zeros(load_length)
 d_p_c = np.zeros(load_length)
 p_c = np.zeros(load_length)
 q_c = np.zeros(load_length)
 p_total = np.zeros(load_length)
 q_total = np.zeros(load_length)
+pc_history = np.zeros(load_length)
 
 # Derived parameters
 OCR = pc_0 / p0  # Over Consolidation Ratio
 
 # Initialize state variables
+K_c = 0
+G_c = 0
 pc = pc_0
 p[0] = p0
 p_total[0] = p0
-e[0] = e0
 sigma = np.array([p0, p0, p0, 0.0, 0.0, 0.0])
 epsilon = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+de_v_c = 0
+de_q_direction = np.zeros(6)
+void_ratio_total[0] = void_ratio_0
+void_ratio_q[0] = void_ratio_0
 
 # Initialize the yield surf surface
 yield_surf = (q[0] ** 2 / M ** 2 + p[0] ** 2) - p[0] * pc
 
 # Loadstep cycle pressure control shear
 for i, eqp_inc in enumerate(eqp_inc_history[:-1]):
+    # Acceleration increment
+    d_eqp_inc = eqp_inc - eqp_inc_history[i - 1] if i > 0 else 0
+
     # Stiffness matrix and strain increment vector and derivatives of the yield function
     De = np.zeros([6, 6])
+    # TODO: HC: check if the stiffness matrix for collisional stress increments can be formulated like elasticity
+    Dc = np.zeros([6, 6])
     df_ds = np.zeros([6, 1])
     df_dep = np.zeros([6, 1])
 
     # Calculate the bulk and shear modulus
     K = V * p[i] / kappa  # Bulk Modulus
     G = (3 * K * (1 - 2 * nu)) / (2 * (1 + nu))  # Shear Modulus
+
+    # Update pre-consolidation pressure
     if yield_surf == 0:
         pc = (q[i] ** 2 / M ** 2 + p[i] ** 2) / p[i]
     else:
         pc = pc_0
+    pc_history[i + 1] = pc
 
     # Elastic Stiffness and other Matrix
+    # Get the quasi-static stress tensor by taking the difference between the total `sigma' and the collisional stress
+    sigma_q = sigma - p_c[i] * np.array([1., 1., 1., 0, 0, 0]) - p_c[i] * M_c * de_q_direction
     for m in range(6):
         for n in range(6):
             if m <= 2:
@@ -94,19 +110,23 @@ for i, eqp_inc in enumerate(eqp_inc_history[:-1]):
                     df_ds[m, 0] = 0
                     df_dep[m, 0] = 0
                 else:
-                    df_ds[m, 0] = (2 * p[i] - pc) / 3 + 3 * (sigma[m] - p[i]) / M ** 2
-                    df_dep[m, 0] = (-p[i]) * pc * (1 + e[i]) / (lambda_val - kappa) * 1
+                    df_ds[m, 0] = (2 * p[i] - pc) / 3 + 3 * (sigma_q[m] - p[i]) / M ** 2
+                    df_dep[m, 0] = (-p[i]) * pc * (1 + void_ratio_q[i]) / (lambda_val - kappa) * 1
                 if m == n:
                     De[m, n] = K + 4 / 3 * G
+                    Dc[m, n] = K_c + 4 / 3 * G_c
                 elif n <= 2:
                     De[m, n] = K - 2 / 3 * G
+                    Dc[m, n] = K_c - 2 / 3 * G_c
             if m > 2:
                 df_ds[m, 0] = 0
                 df_dep[m, 0] = 0
                 if m == n:
                     De[m, n] = G
+                    Dc[m, n] = G_c
                 else:
                     De[m, n] = 0
+                    Dc[m, n] = 0
 
         # If the yield surface is negative, the stiffness matrix is elastic
         if yield_surf < 0:
@@ -114,7 +134,7 @@ for i, eqp_inc in enumerate(eqp_inc_history[:-1]):
         # If the yield surface is positive, the stiffness matrix is elastic-plastic
         else:
             D = De - (De.dot(df_ds).dot(df_ds.T).dot(De)) / (
-                    -(df_dep.T).dot(df_ds) + (df_ds.T).dot(De).dot(df_ds))
+                    -(df_dep.T).dot(df_ds) + (df_ds.T).dot(De).dot(df_ds)) + Dc
 
     # Fill the strain increment vector
     if deformation_mode == 'drained':
@@ -122,50 +142,62 @@ for i, eqp_inc in enumerate(eqp_inc_history[:-1]):
         d_epsilon = np.array(
             [eqp_inc, - D[1, 0] / (D[1, 1] + D[1, 2]) * eqp_inc, - D[2, 0] / (D[2, 1] + D[2, 2]) * eqp_inc, 0., 0., 0.])
     elif deformation_mode == 'undrained':
-        d_epsilon = np.array([eqp_inc, -eqp_inc / 2., -eqp_inc / 2., 0., 0., 0.])
+        d_epsilon = np.array(
+            [eqp_inc, -eqp_inc / 2., -eqp_inc / 2., 0., 0., 0.])
+
+    # Get the direction of the deviatoric strain
+    de_v = np.sum(d_epsilon[:3])
+    de_q = d_epsilon - np.array([1., 1., 1., 0, 0, 0]) * de_v
+    de_q_norm = np.sqrt(3. / 2. * de_q.dot(de_q))
+    de_q_direction = de_q / de_q_norm
+
+    # Calculate collisional stress
+    if d_eqp_inc != 0:
+        # Get sign of the acceleration rate, positive: accelerate; negative decelerate.
+        # Now simplified as sign(d_eqp_inc). Should have been the sign between de_q_direction and e_q_direction tensors
+        sign = np.sign(d_eqp_inc * eq[i])
+
+        Phi = 1.0 / (1.0 + void_ratio_q[i])
+        de_v_c = - Phi_0 / Phi ** 2 * Delta_Phi * sign * de_q_norm
+        d_p_c[i + 1] = p_total[i] / (lambda_val * Phi ** 2) * Delta_Phi * sign * de_q_norm
+
+        K_c = d_p_c[i + 1] / de_v_c
+        G_c = d_p_c[i + 1] * M_c / de_q_norm
+        p_c[i + 1] = p_c[i] + d_p_c[i + 1]
+        q_c[i + 1] = q_c[i] + d_p_c[i + 1] * M_c
+    else:
+        p_c[i + 1] = p_c[i]
+        q_c[i + 1] = q_c[i]
+        K_c = 0
+        G_c = 0
 
     # Update stress
     d_sigma = D.dot(d_epsilon)
-    sigma += d_sigma
-    epsilon += d_epsilon
+    sigma += d_sigma  # + d_p_c[i + 1] * np.array([1., 1., 1., 0, 0, 0]) + d_p_c[i + 1] * M_c * de_q_direction
+    epsilon += d_epsilon  # + 1. / 3 * de_v_c * np.array([1., 1., 1., 0, 0, 0])
+    # Update stress invariants
+    p_total[i + 1] = np.sum(sigma[:3]) / 3.0
+    p_s = sigma - np.array([1., 1., 1., 0, 0, 0]) * p_total[i + 1]
+    q_total[i + 1] = np.sqrt(3. / 2. * p_s.dot(p_s))
 
     # Compute stress and strain invariants
     ev[i + 1] = np.sum(epsilon[:3])
     epsilon_s = epsilon - np.array([1, 1, 1, 0, 0, 0]) * ev[i + 1]
-    # eq[i + 1] = np.sqrt(2. * epsilon_s.dot(epsilon_s))
-    eq[i + 1] = 2./3. * (epsilon[0] - epsilon[2])
+    eq[i + 1] = np.sqrt(2. / 3. * epsilon_s.dot(epsilon_s))
 
-    p[i + 1] = np.sum(sigma[:3]) / 3.0
-    p_s = sigma - np.array([1., 1., 1., 0, 0, 0]) * p[i + 1]
-    # q[i + 1] = np.sqrt(1. / 2. * p_s.dot(p_s))
-    q[i + 1] = sigma[0] - sigma[2]
-    u[i + 1] = p0 + q[i + 1] / 3. - p[i + 1]
+    p[i + 1] = p_total[i + 1] - p_c[i + 1]
+    q[i + 1] = q_total[i + 1] - q_c[i + 1]
+    u[i + 1] = p0 + q_total[i + 1] / 3. - p_total[i + 1]
 
     # Update specific volume
     V = N - (lambda_val * np.log(pc)) + (kappa * np.log(pc / p[i + 1]))
-    e[i + 1] = V - 1.0
+    void_ratio_q[i + 1] = V - 1
+    void_ratio_total[i + 1] = void_ratio_0 - (1 + void_ratio_0) * ev[i + 1]
 
     if yield_surf < 0:
         yield_surf = q[i + 1] ** 2 + M ** 2 * p[i + 1] ** 2 - M ** 2 * p[i + 1] * pc
     else:
         yield_surf = 0
-
-    # # Normal Consolidation Line (NCL)
-    # pNCL = np.linspace(1, pc, nIter)
-    # qNCL = M * pNCL
-    # eNCL = (N - lambda_val * np.log(pNCL)) - 1
-    #
-    # # Critical State Line (CSL)
-    # pCSL = pNCL
-    # Gamma = 1 + e[i] + lambda_val * np.log(p[i])
-    # eCSL = (Gamma - lambda_val * np.log(pCSL)) - 1
-    #
-    # # Final Yield Surface
-    # # CSL in p-q space
-    # p_fyield = np.linspace(1, pc, nIter)
-    # q_fyield = M * p_fyield
-    # # Plot the final yield locus
-    # qyf = (M ** 2 * (pc * p_fyield - p_fyield ** 2)) ** 0.5
 
 # Display results
 plt.figure('Pressure controlled simple shear')
@@ -189,9 +221,9 @@ plt.xlabel('Time [s]')
 plt.ylabel('Volumetric strain [-]')
 
 plt.subplot(2, 4, 4)
-plt.plot(np.arange(load_length) * dt, psi, '-b')
+plt.plot(np.arange(load_length) * dt, pc_history, '-b')
 plt.xlabel('Time [s]')
-plt.ylabel('State variable (psi) [-]')
+plt.ylabel('Pre-consolidation pressure [kPa]')
 
 plt.subplot(2, 4, 5)
 plt.plot(np.arange(load_length) * dt, p_c, '-b')
@@ -207,10 +239,10 @@ plt.ylabel('Deviatoric stress [kPa]')
 plt.legend()
 
 plt.subplot(2, 4, 7)
-plt.plot(p, e, '-b', label='Quasi-static')
-plt.plot(p_total, e, '-g', label='Total')
+plt.plot(p, void_ratio_q, '-b', label='Quasi-static')
+plt.plot(p_total, void_ratio_total, '-g', label='Total')
 plt.xlabel('Volumetric stress [kPa]')
-plt.ylabel('void ratio [-]')
+plt.ylabel('Void ratio [-]')
 plt.legend()
 
 plt.subplot(2, 4, 8)
---
+++ MCC_quasi.py
@@ -0,0 +1,153 @@
+# encoding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Input Parameters for Modified Cam-Clay:');
+
+cp=input('Enter the inital Consolidation pressure (kPa] (eg., 150 kPa]  = ');
+cp=150 if cp == '' else float(cp)
+p0=input('Enter the initial Confining pressure (kPa]    (eg., 150 kPa]  = ');
+p0=150 if p0 == '' else float(p0)
+M=input( 'Enter the value of Critical Friction Angle M  (eg., 0.95)     = ');
+M=0.95 if M == '' else float[m]
+l=input( 'Enter the value of Lamda                      (eg., 0.2)      = ');
+l=0.2 if l == '' else float(l)
+k=input( 'Enter the value of Kappa                      (eg., 0.04)     = ');
+k=0.04 if k == '' else float(k)
+N=input( 'Enter the value of N                          (eg., 2.5)      = ');
+N=2.5 if N == '' else float(N)
+nu=input('Enter the value of poissons ratio             (eg., 0.15)     = ');
+nu=0.15 if nu == '' else float(nu)
+analysis = input('Enter the type of Analysis: [1] Triaxial Drained [2] Triaxial Undrained = ');
+analysis=1 if analysis == '' else float(analysis)
+
+if analysis==1:
+	print('Triaxial Drained Simulation is in progress ...');
+elif analysis==2:
+	print('Triaxial Undrained Simulation is in progress ...');
+else: exit();
+
+# Computation of Other Parameters (V,e0 and OCR)
+pc=cp;
+V=N-(l*np.log(pc))+(k*np.log(pc/p0)); # Specific Volume
+e0=V-1;                         # Initial Void Ratio
+OCR=cp/p0,                      # Over Consolidation Ratio
+
+# Strain Increament and Strain Matrix Definition
+print('Strain increament and nIteration:');
+nIter=10000; de=1e-4;
+
+# Block Memory allocation
+De=np.zeros([6,6]);      # Stiffness Matrix
+dfds=np.zeros([6,1]);
+dfdep=np.zeros([6,1]);
+dStrain=np.zeros([6,1]); # Increamental Strain
+
+u=np.zeros([nIter+1,1]);    # Pore Water Pressure
+p=np.zeros([nIter+1,1]);    # Mean Effective Stress
+q=np.zeros([nIter+1,1]);    # Deviatoric Stress
+void=np.zeros([nIter+1,1]); # Void ratio
+epsV=np.zeros([nIter+1,1]); # Volumetic Strain
+epsD=np.zeros([nIter+1,1]); # Deviatoric Strain
+
+# yieldSurf Surface and Conditions
+p_ini_yield=np.linspace(0,pc,nIter);
+q_ini_yield = M*p_ini_yield;
+qyf_ini=(M**2*(pc*p_ini_yield-p_ini_yield**2))**0.5;
+
+# Initialize
+a=0;									# nIterator
+S=np.array([p0+1.0e-10,p0,p0,0,0,0])	# Stress 
+strain=np.array([0,0,0,0,0,0])			# Strain
+p[a]=(S[0]+2*S[2])/3;                
+q[a]=(S[0]-S[1]);
+yieldSurf=(q[a]**2/M**2+p[a]**2)-p[a]*pc; # Defining the yieldSurf surface
+void[a]=e0
+
+# CamClay nIteration for normal and over consolidated clay& Inside/Outside yieldSurf
+while a<nIter:
+	K=V*p[a]/k;                       # Bulk Modulus
+	G=(3*K*(1-2*nu))/(2*(1+nu));      # Shear Modulus
+	if yieldSurf==0: pc=(q[a]**2/M**2+p[a]**2)/p[a]; 
+	else: pc=cp
+
+	# Elastic Stiffness and other Matrix 
+	for m in range(6):
+	   for n in range(6):
+		   if m <= 2:
+			   if yieldSurf < 0:
+				   dfds[m,0]=0;
+				   dfdep[m,0]=0
+			   else:
+				   dfds[m,0]=(2*p[a]-pc)/3 + 3*(S[m]-p[a])/M**2; # df/ds
+				   dfdep[m,0]=(-p[a])*pc*(1+void[a])/(l-k)*1; # df/dep
+			   if m==n:
+				   De[m,n]=K+4/3*G; # Elastic Stiffness
+			   elif n<=2:
+				   De[m,n]=K-2/3*G;
+		   if m>2:
+			   dfds[m,0]=0;
+			   dfdep[m,0]=0;
+			   if m==n: De[m,n]= G;  # Elastic Stiffness
+			   else: De[m,n]=0;
+
+	#Stiffness Matrix  
+	if yieldSurf<0: D=De; #Elastic
+	else: D=De-(De.dot(dfds).dot(dfds.T).dot(De))/(-(dfdep.T).dot(dfds)+(dfds.T).dot(De).dot(dfds)); #Plastic
+	#~ if a%(nIter/20) == 0:
+		#~ print '(dfdep.T).dot(dfds):%.f; -p[a]*V*pc/(l-k)*(2*p[a]-pc):%.f'%((dfdep.T).dot(dfds),-p[a]*V*pc/(l-k)*(2*p[a]-pc))
+
+	#Stress and Strain Updates
+	if analysis==1: #Triaxial Drained (?)
+	   dStrain = np.array([de,-1*D[1,0]/(D[1,1]+D[1,2])*de,-1*D[2,0]/(D[2,1]+D[2,2])*de,0.,0.,0.]);
+	elif analysis==2: #Triaxial Undrained
+	   dStrain = np.array([de,-de/2.,-de/2.,0.,0.,0.]);
+
+	dS=D.dot(dStrain);
+	S=S+dS;
+	strain=strain+dStrain;
+
+	depsV = dStrain[0] + dStrain[1] + dStrain[2]; # Increamental Volumetric Strain
+	depsD = 2./3. * (dStrain[0] - dStrain[2]);      # Increamental Deviatoric Strain
+
+	#Update Specific Volume
+	V=N-(l*np.log(pc))+(k*np.log(pc/p[a]));
+
+	#Subsequent cycle update
+	a=a+1;
+	p[a]=(S[0]+S[1]+S[2])/3;
+	q[a]=S[0]-S[2];
+	u[a]=p0+q[a]/3-p[a];
+
+	void[a] = V-1.0;
+	epsV[a] = epsV[a-1] + depsV;
+	epsD[a] = epsD[a-1] + depsD;
+
+	if yieldSurf<0: yieldSurf=q[a]**2+M**2*p[a]**2-M**2*p[a]*pc;
+	else: yieldSurf=0;
+   
+	# Normal Consolidation Line (NCL)
+	pNCL = np.linspace(1,pc,nIter)
+	qNCL = M * pNCL;
+	eNCL = (N - l*np.log(pNCL)) - 1;
+     
+	# Critical State Line (CSL)
+	pCSL = pNCL;
+	Gamma = 1+void[a]+l*np.log(p[a]);
+	eCSL = (Gamma - l*np.log(pCSL)) - 1;
+     
+	# Final Yield Surface
+	# CSL in p-q space
+	p_fyield = np.linspace(1,pc,nIter);
+	q_fyield = M*p_fyield;
+	# Plot the final yield locus
+	qyf = (M**2*(pc*p_fyield-p_fyield**2))**0.5;
+	
+	if a%(nIter/200) == 0:
+		plt.plot(np.array([0,2.0*p[a]]),np.array([0,2.0*q[a]]),'k--')
+		plt.plot(p[p.nonzero()],q[q.nonzero()],'r',label='stress path');
+		plt.plot(p_fyield,q_fyield,'k');
+		plt.plot(p_fyield,qyf,'b-');
+		plt.plot(p_ini_yield,qyf_ini,'g');
+		plt.xlabel('p'); plt.xlabel('q'); 
+		plt.show()
